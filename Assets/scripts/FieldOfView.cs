0b716f6a2f8572366001dc24537a8e6d93fa425c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-09 21:33:31 +0000

    Reduce raycasts required for accurate mesh with a FindEdge method

--- Assets/scripts/FieldOfView.cs
+++ Assets/scripts/FieldOfView.cs
@@ -19,6 +19,7 @@ public class FieldOfView : MonoBehaviour {
     [SerializeField]
     private MeshFilter viewMeshFilter;
     private Mesh viewMesh;
+    private int edgeResolveIterations = 5;
 
     private void Start ()
     {
@@ -77,12 +78,30 @@ public class FieldOfView : MonoBehaviour {
         int stepCount = Mathf.RoundToInt (viewAngle * meshResolution);
         float stepAngleSize = viewAngle / stepCount;
         List<Vector3> viewPoints = new List<Vector3> ();
+        ViewCastInfo oldViewCast = new ViewCastInfo ();
 
         for (int i = 0; i <= stepCount; i++)
         {
             float angle = transform.eulerAngles.y - viewAngle / 2f + stepAngleSize * i;
             ViewCastInfo newViewCast = ViewCast (angle);
+
+            if (i > 0)
+            {
+                if (oldViewCast.hit != newViewCast.hit)
+                {
+                    EdgeInfo edge = FindEdge (oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add (edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add (edge.pointB);
+                    }
+                }
+            }
             viewPoints.Add (newViewCast.point);
+            oldViewCast = newViewCast;
         }
 
         int vertexCount = viewPoints.Count + 1;
@@ -123,6 +142,34 @@ public class FieldOfView : MonoBehaviour {
         }
     }
 
+    private EdgeInfo FindEdge (ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2f;
+            ViewCastInfo newViewCast = ViewCast (angle);
+            
+            if (newViewCast.hit == minViewCast.hit)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+
+        return new EdgeInfo (minPoint, maxPoint);
+    }
+
     public struct ViewCastInfo {
         public bool hit;
         public Vector3 point;
@@ -135,6 +182,17 @@ public class FieldOfView : MonoBehaviour {
             point = _point;
             dist = _dist;
             angle = _angle;
+        }
+    }
+
+    public struct EdgeInfo {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo (Vector3 _pointA, Vector3 _pointB)
+        {
+            pointA = _pointA;
+            pointB = _pointB;
         }
     }
 
