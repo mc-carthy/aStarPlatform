a10e00adc7903ae35571ce83d4c75d2d2298c373
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-09 20:23:47 +0000

    Add scene visualisation of player view radius

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor {
+
+	private void OnSceneGUI ()
+    {
+        FieldOfView fow = (FieldOfView) target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc (fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+    }
+
+}
--- Assets/scripts/FieldOfView.cs
+++ Assets/scripts/FieldOfView.cs
@@ -2,6 +2,12 @@
 
 public class FieldOfView : MonoBehaviour {
 
-	
+    public float viewRadius;
+    public float viewAngle;
+
+    public Vector3 DirFromAngle (float angleInDegrees)
+    {
+        return new Vector3 (Mathf.Sin (angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos (angleInDegrees * Mathf.Deg2Rad));
+    }
 
 }
