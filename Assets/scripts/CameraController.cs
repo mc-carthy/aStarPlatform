31e079a40234600575399b81f069d524dd92f137
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-01 15:04:36 +0000

    Camera follows cursor around player

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -4,16 +4,19 @@ public class CameraController : MonoBehaviour {
 
 	[SerializeField]
 	private Transform target;
-	private Vector3 startPos;
+	private float maxCamDistFromTarget = 15f;
+	private float maxCamDistFromTargetFar = 20f;
 
-	private void Start () {
-		startPos = transform.position;
-	}
 
 	private void Update () {
-		Vector3 temp = startPos;
-		temp.x = target.position.x;
-		temp.z = target.position.z;
-		transform.position = temp;
+		float mousePosX = Input.mousePosition.x - (Screen.width / 2f);
+		float mousePosY = Input.mousePosition.y - (Screen.height / 2f);
+
+		Vector3 dirToCursor = new Vector3 (mousePosX, transform.position.y, mousePosY) - target.transform.position;
+
+		maxCamDistFromTarget = Mathf.Min (dirToCursor.magnitude, maxCamDistFromTarget);
+		dirToCursor.y = 10f;
+
+		transform.position = target.transform.position + dirToCursor.normalized * maxCamDistFromTarget;
 	}
 }
